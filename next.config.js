57d8bf990c395ccbf2dd65ec4cac67289333be84
Gaurav Omraj Nagoshe <41421090+Mrgon96@users.noreply.github.com>
2023-12-10 15:05:34 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-module.exports = {
-  trailingSlash: true,
-  output: 'export',
-    env: {
-      customKey: 'my-value',
-      NOTION_SECRET_KEY: process.env.NOTION_SECRET_KEY, 
-      NOTION_DB_ID: process.env.NOTION_DB_ID, 
-    },
-  }
+
+const nextConfig = {
+trailingSlash: true,
+output: 'export',
+env: {
+  customKey: 'my-value',
+  NOTION_SECRET_KEY: process.env.NOTION_SECRET_KEY, 
+  NOTION_DB_ID: process.env.NOTION_DB_ID, 
+},
+}
+module.exports = nextConfig
